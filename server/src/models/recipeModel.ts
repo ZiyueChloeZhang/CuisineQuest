d605a332dc12ea9eee93a9e5fe2b636756f7eb45
allyoukneadisloaf <71418763+allyoukneadisloaf@users.noreply.github.com>
2023-11-19 11:37:51 -0800

    Edited recipe schema.

--- server/src/models/recipeModel.ts
+++ server/src/models/recipeModel.ts
@@ -6,9 +6,18 @@ interface IRecipe extends Document {
 }
 
 // TODO
+
 const RecipeSchema: Schema = new Schema({
+  id: { type: String, required: true},
+  name: { type: String, required: true },
+  time: { hours: { type: Number, required: true }, 
+  minutes: { type: Number, required: true } },
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  servings: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  ingredients: [{ type: String, required: true }],
+  tags: [{ type: String, required: true }],
+  restrictions: [{ type: String, required: true }]
 });
 
 const Recipe = mongoose.model<IRecipe>('Recipe', RecipeSchema);
